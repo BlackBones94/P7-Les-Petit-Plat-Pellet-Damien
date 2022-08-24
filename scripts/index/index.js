67b2c22ada00a3869d37858a0ca367acd19269df
Damien <damien.pellet94@hotmail.fr>
2022-08-24 16:03:39 +0200

    ajout de la css et mise en place du js et du css pour les recipes

--- scripts/index/index.js
+++ scripts/index/index.js
@@ -1,1 +1,25 @@
+import {recipes} from "../data/recipes.js";
+import {CardRecip} from "../facto/recipes.js";
 
+console.log(recipes)
+
+function displayRecipes(recipes) {
+    const cardsSection = document.querySelector(".card-section");
+    cardsSection.innerHTML= '';
+    recipes.forEach((item) => {
+        const recipesTemplates = new CardRecip(item);
+        const cardDom = recipesTemplates.getCardDom();
+        cardsSection.innerHTML += cardDom;
+// ne pas oublier le +
+        console.log(CardRecip)
+    });
+}
+
+function init(){
+    displayRecipes(recipes);
+}
+
+init();
+
+// trier liste alpha 
+// masquer dropdown a la recherche 
